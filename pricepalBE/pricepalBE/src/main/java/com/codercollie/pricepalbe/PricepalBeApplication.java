4296e657d74044ef5bf00a3266d8cfae4a6c1e16
Coder Collie <pawgrammer.collie@gmail.com>
2025-11-06 19:56:23 +0100

    :DEV: add basic folders structure for projects, readme and gitignore

---
+++ pricepalBE/pricepalBE/src/main/java/com/codercollie/pricepalbe/PricepalBeApplication.java
@@ -0,0 +1,13 @@
+package com.codercollie.pricepalbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricepalBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricepalBeApplication.class, args);
+	}
+
+}
---
+++ pricepalBE/pricepalBE/src/test/java/com/codercollie/pricepalbe/PricepalBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codercollie.pricepalbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricepalBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
