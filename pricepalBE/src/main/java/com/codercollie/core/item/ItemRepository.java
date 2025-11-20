165f5c58ab1282ca384b09430611b768716062bf
Coder Collie <pawgrammer.collie@gmail.com>
2025-11-20 15:24:21 +0100

    :DEV: Pagination first-step extending JpaSpecificationExecutor

--- pricepalBE/src/main/java/com/codercollie/core/item/ItemRepository.java
+++ pricepalBE/src/main/java/com/codercollie/core/item/ItemRepository.java
@@ -1,7 +1,8 @@
 package com.codercollie.core.item;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ItemRepository extends JpaRepository<Item, Long> {
+public interface ItemRepository extends JpaRepository<Item, Long>, JpaSpecificationExecutor<Item> {
 
 }
