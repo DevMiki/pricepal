c8e24cb36cc8da6562cd2f989cd3c0605491eda1
CoderCollie <pawgrammer.collie@gmail.com>
2025-11-22 18:37:44 +0100

    :DEV: Pagination FE first-step: add new item-filter component

---
+++ PricepalFE/src/app/items/item-filter/item-filter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-filter',
+  standalone: true,
+  imports: [],
+  templateUrl: './item-filter.component.html',
+  styleUrl: './item-filter.component.scss'
+})
+export class ItemFilterComponent {
+
+}
