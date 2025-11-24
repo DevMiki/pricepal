9a0474ca626ed70328c63e059e2bc2e4862e9b10
CoderCollie <pawgrammer.collie@gmail.com>
2025-11-24 19:28:51 +0100

    :DEV: Add item feature third-step, add form in add-item component, still to wire on submit

--- PricepalFE/src/app/items/add-item/add-item.component.ts
+++ PricepalFE/src/app/items/add-item/add-item.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { RouterLink } from "@angular/router";
+
+type FieldKey = 'name' | 'price' | 'supermarket' | 'notes'
 
 @Component({
   selector: 'app-add-item',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, RouterLink],
   templateUrl: './add-item.component.html',
   styleUrl: './add-item.component.scss'
 })
 export class AddItemComponent {
 
+  private formBuilder = inject(FormBuilder)
+
+  fields: Array<{
+    key: FieldKey,
+    label: string,
+    type?: 'text' | 'number' | 'textarea';
+    required?: boolean;
+    min?: number;
+    step?: number;
+  }> = [
+    {key: 'name', label: 'Name', required: true },
+    {key: 'price', label: 'Price', type: 'number', required: true, min: 0, step: 0.01 },
+    {key: 'supermarket', label: 'Supermarket', required: true},
+    {key: 'notes', label: 'Notes', type: 'textarea'}
+  ]
+
+  form: FormGroup = this.formBuilder.group({
+    name: ['', [Validators.required]],
+    price: ['', [Validators.required, Validators.min(0)]],
+    supermarket: ['', [Validators.required]],
+    notes: ['']
+  })
+
+  onSubmit(){
+    if(this.form.invalid){
+      this.form.markAllAsTouched()
+    }
+  }
 }
